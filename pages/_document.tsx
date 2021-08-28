2a656c7b67cbaf8956e4185985c620e2b07099c3
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2021-08-28 13:47:07 -0700

    description change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,9 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta property="og:title" content="Joseph Alvarenga Beech - Full Stack Developer" />
-          <meta property="og:image" content="%PUBLIC_URL%/logo512.png" />
-          <link rel='icon' href='/favicon.ico' />
+          <meta property="og:title" content="Joseph Alvarenga Beech - Software Engineer/Full-Stack development" />
+          <meta property="og:image" content="/public/favicon.ico" />
+          <link rel='icon' href='/public/favicon.ico' />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
